360ecf21734aee4a40b2b211defd9dd9d5ccbdd9
Juanma <35576283+jmmolins87@users.noreply.github.com>
2024-01-30 14:45:47 +0100

    remove all

--- src/app/country/pages/selector-page/selector-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-selector-page',
-  templateUrl: './selector-page.component.html'
-})
-export class SelectorPageComponent {
-
-}
